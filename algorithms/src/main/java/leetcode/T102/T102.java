9af0a724fa89e7b71a8285f4da9592928d2a2aa2
out-sharipov.f.b <OUT-Sharipov.F.B@sbertech.ru>
2026-08-13 01:51:31 +0500

    problem-102: solve leetcode problem level order traversal

---
+++ algorithms/src/main/java/leetcode/T102/T102.java
@@ -0,0 +1,51 @@
+package leetcode.T102;
+
+import leetcode.T104.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Given the root of a binary tree,
+ * return the level order traversal of its nodes' values.
+ * (i.e., from left to right, level by level).
+ * <p>
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: [[3],[9,20],[15,7]]
+ */
+public class T102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            var queue_size = queue.size();
+            List<Integer> currentArray = new ArrayList<>();
+
+            for (int i = 0; i < queue_size; i++) {
+                TreeNode currentNode = queue.poll();
+
+                if (currentNode != null) {
+                    currentArray.add(currentNode.getVal());
+
+                    if (currentNode.getLeft() != null) {
+                        queue.offer(currentNode.getLeft());
+                    }
+                    if (currentNode.getRight() != null) {
+                        queue.offer(currentNode.getRight());
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
